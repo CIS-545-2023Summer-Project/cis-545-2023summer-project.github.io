e8efaa70e860870821649e65bc1b3da463cce270
Guan He <heguanelvis@gmail.com>
2023-08-12 17:23:38 -0400

    added map legend

--- script.js
+++ script.js
@@ -13,19 +13,18 @@ document.addEventListener("DOMContentLoaded", event => {
       document.getElementById("tooltip-text").style.visibility = "hidden";
     });
 
+  // Mapping
   mapboxgl.accessToken =
     "pk.eyJ1IjoiaGVndWFuZWx2aXMiLCJhIjoiY2xnb25mZXI2MGo2NjNua2NyY2UwZWZsNSJ9.In4gfheRxQlTYDAc9g0JiA";
 
   const map = new mapboxgl.Map({
     container: "map",
-    style: "mapbox://styles/mapbox/dark-v10",
+    style: "mapbox://styles/mapbox/dark-v11",
     center: [139.78, 35.5895],
     zoom: 8,
   });
 
-  // Mapping
   d3.csv("data/municipal_avg_price.csv").then(priceData => {
-    const maxPrice = d3.max(priceData, d => +d.AveragePricePerSQM);
     const prices = priceData.map(d => +d.AveragePricePerSQM);
     const priceMap = {};
 
@@ -33,14 +32,18 @@ document.addEventListener("DOMContentLoaded", event => {
       priceMap[d.JCODE] = d;
     });
 
-    // Define a color scale for 20% buckets
     const custom_color_scale = [
-      "#f2f0f7",
-      "#cbc9e2",
-      "#9e9ac8",
-      "#756bb1",
-      "#54278f",
+      "#ffe0cc",
+      "#ffc299",
+      "#ffa366",
+      "#ff8533",
+      "#ff7519",
+      "#cc5200",
+      "#993d00",
+      "#662900",
+      "#331400",
     ];
+
     const colorScale = d3
       .scaleQuantile()
       .domain(prices)
@@ -50,18 +53,47 @@ document.addEventListener("DOMContentLoaded", event => {
       .quantiles()
       .map((q, i) => [q, custom_color_scale[i]]);
 
+    const quantiles = [
+      Math.min(...prices),
+      ...colorScale.quantiles(),
+      Math.max(...prices),
+    ];
+
+    const legend = document.getElementById("map-legend");
+
+    const title = document.createElement("h6");
+    title.textContent = "Avg Price per SQM";
+    legend.appendChild(title);
+
+    custom_color_scale.forEach((color, i) => {
+      const legendItem = document.createElement("div");
+      legendItem.className = "legend-item";
+
+      const legendColor = document.createElement("div");
+      legendColor.className = "legend-color";
+      legendColor.style.backgroundColor = color;
+      legendItem.appendChild(legendColor);
+
+      const min = parseInt(quantiles[i]).toLocaleString();
+      const max = parseInt(quantiles[i + 1]).toLocaleString();
+      const legendText = document.createTextNode(`${min} - ${max} yen`);
+      legendItem.appendChild(legendText);
+
+      legend.appendChild(legendItem);
+    });
+
     d3.json("data/GTA_Municipalities.geojson").then(geoData => {
-      // Filter GeoJSON features based on CSV data
       geoData.features = geoData.features.filter(
         feature => priceMap[feature.properties.JCODE]
       );
 
-      // Add price data to GeoJSON features
       geoData.features.forEach(feature => {
         const jcode = feature.properties.JCODE;
         if (priceMap[jcode]) {
           feature.properties.AveragePricePerSQM =
             +priceMap[jcode].AveragePricePerSQM;
+          feature.properties.Prefecture = priceMap[jcode].Prefecture;
+          feature.properties.Municipality = priceMap[jcode].Municipality;
         }
       });
 
@@ -85,6 +117,38 @@ document.addEventListener("DOMContentLoaded", event => {
             ],
             "fill-opacity": 0.5,
           },
+        });
+
+        map.on("mousemove", "municipalities-layer", function (e) {
+          const feature = e.features[0];
+
+          const prefecture = feature.properties.Prefecture;
+          const municipality = feature.properties.Municipality;
+          const averagePrice = feature.properties.AveragePricePerSQM;
+          const formattedAveragePrice = averagePrice.toLocaleString();
+
+          const tooltipContent = `
+            <h6>${prefecture}</h6>
+            <span>Municipality: ${municipality}</span>
+            <br/>
+            <span>Avg Price per SQM: ${formattedAveragePrice} yen</span>
+          `;
+
+          const tooltip = document.getElementById("map-tooltip");
+          tooltip.innerHTML = tooltipContent;
+
+          let left = e.originalEvent.clientX;
+          let top = e.originalEvent.clientY;
+
+          tooltip.style.left = left + "px";
+          tooltip.style.top = top + "px";
+          tooltip.style.visibility = "visible";
+        });
+
+        map.on("mouseleave", "municipalities-layer", function () {
+          const tooltip = document.getElementById("map-tooltip");
+          tooltip.innerHTML = "";
+          tooltip.style.visibility = "hidden";
         });
       });
     });
