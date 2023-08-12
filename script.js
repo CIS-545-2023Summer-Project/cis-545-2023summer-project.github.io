15bb4a03ab441dd23b04d487160462e82df760b5
Guan He <heguanelvis@gmail.com>
2023-08-12 01:05:23 -0400

    layout updates

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    
+document.addEventListener("DOMContentLoaded", event => {
+  console.log("DOM fully loaded and parsed");
+
+  document
+    .getElementById("colab-btn")
+    .addEventListener("mouseenter", function () {
+      document.getElementById("tooltip-text").style.visibility = "visible";
+    });
+
+  document
+    .getElementById("colab-btn")
+    .addEventListener("mouseleave", function () {
+      document.getElementById("tooltip-text").style.visibility = "hidden";
+    });
 });
